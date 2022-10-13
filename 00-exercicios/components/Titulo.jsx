89f6138b59feadf0ee88b3d62421a5b3dc7243b9
heltonricardo <helton.ricardo13@gmail.com>
2022-10-13 14:11:30 -0300

    passando parametros para componente

---
+++ 00-exercicios/components/Titulo.jsx
@@ -0,0 +1,8 @@
+export default ({ principal, descricao }) => {
+  return (
+    <>
+      <h1>{principal}</h1>
+      <h2>{descricao}</h2>
+    </>
+  );
+};
---
+++ 00-exercicios/pages/basicos/usando-titulo.jsx
@@ -0,0 +1,11 @@
+import Titulo from "../../components/Titulo";
+
+export default () => {
+  return (
+    <>
+      <Titulo principal="T1" descricao="S1" />
+      <Titulo principal="T2" descricao="S2" />
+      <Titulo principal="T3" descricao="S3" />
+    </>
+  );
+};
