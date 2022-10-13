bfd9cdc728d15526419e3860ad281813b40fd5f8
heltonricardo <helton.ricardo13@gmail.com>
2022-10-13 14:12:13 -0300

    usando estilo global

---
+++ 00-exercicios/pages/basicos/estilos/integracao01.jsx
@@ -0,0 +1,11 @@
+// Importado em _app.js
+
+export default () => {
+  return (
+    <>
+      <div className="vermelha">Texto #01</div>
+      <div className="verde">Texto #02</div>
+      <div className="azul">Texto #03</div>
+    </>
+  );
+};
