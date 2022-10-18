2416a4fe799bbd6bf73fccc4b2d680b577a992bb
heltonricardo <helton.ricardo13@gmail.com>
2022-10-18 14:44:24 -0300

    componente condicional

---
+++ 00-exercicios/components/If/index.jsx
@@ -0,0 +1,1 @@
+export default ({ condicao, children }) => condicao && children;
---
+++ 00-exercicios/pages/render/condicional02.jsx
@@ -0,0 +1,25 @@
+import If from "../../components/If";
+
+const isPar = (n) => n % 2 === 0;
+const isImpar = (n) => n % 2 !== 0;
+
+function a() {
+    a == 
+}
+
+export default () => (
+  <>
+    {[...Array(10).keys()]
+      .map((n) => n + 1)
+      .map((n) => (
+        <>
+          <If key={n} condicao={isPar(n)}>
+            <h1>{n} é par!</h1>
+          </If>
+          <If key={n} condicao={isImpar(n)}>
+            <h1>{n} é ímpar!</h1>
+          </If>
+        </>
+      ))}
+  </>
+);
