7256f929da0ebb750f7df1abf4c0d8192c4914dd
heltonricardo <helton.ricardo13@gmail.com>
2022-10-28 11:53:25 -0300

    iniciando componente porta

---
+++ 03-monty-hall/src/components/Porta/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+export default function Porta({}) {
+  return (
+    <div className={styles.area}>
+      <div className={styles.batente}>
+        <div className={styles.porta}>
+          <div className={styles.numero}>3</div>
+        </div>
+      </div>
+    </div>
+  );
+}
