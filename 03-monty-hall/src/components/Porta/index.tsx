335462df7caa6e6aea1284ddbdb53dca229f5eca
heltonricardo <helton.ricardo13@gmail.com>
2022-11-01 09:26:11 -0300

    finalizando componente porta

--- 03-monty-hall/src/components/Porta/index.tsx
+++ 03-monty-hall/src/components/Porta/index.tsx
@@ -7,16 +7,25 @@ interface Props {
 }
 
 export default function Porta({ value: porta, onChange: evento }: Props) {
-  const selecao = porta.isSelecionada ? styles.selecao : "";
+  const selecao = porta.isSelecionada && !porta.isAberta ? styles.selecao : "";
+
+  function abrirPorta(e: React.MouseEvent<HTMLElement>) {
+    e.stopPropagation();
+    evento(porta.abrir());
+  }
+
+  function renderizarPorta() {
+    return (
+      <div className={styles.porta}>
+        <div className={styles.numero}>{porta.numero}</div>
+        <div className={styles.macaneta} onClick={abrirPorta}></div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.area} onClick={() => evento(porta.alternarSelecao())}>
-      <div className={`${styles.batente} ${selecao}`}>
-        <div className={styles.porta}>
-          <div className={styles.numero}>{porta.numero}</div>
-          <div className={styles.macaneta}></div>
-        </div>
-      </div>
+      <div className={`${styles.batente} ${selecao}`}>{porta.isAberta || renderizarPorta()}</div>
       <div className={styles.chao}></div>
     </div>
   );
