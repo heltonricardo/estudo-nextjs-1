3653c9c84fd46637d7e6f73349cf569efa7a1efc
heltonricardo <helton.ricardo13@gmail.com>
2022-11-03 16:47:47 -0300

    estilo e validacao dos valores

--- 03-monty-hall/src/pages/jogo/[...params].tsx
+++ 03-monty-hall/src/pages/jogo/[...params].tsx
@@ -7,8 +7,17 @@ import styles from "./styles.module.scss";
 
 export default function Jogo() {
   const router = useRouter();
-
   const [portas, setPortas] = useState([]);
+  const [isValido, setIsValido] = useState(false);
+
+  useEffect(() => {
+    if (router.query?.params) {
+      const [qntPortas, portaPremiada] = router.query.params;
+      const qtdeValida = 3 <= +qntPortas && +qntPortas <= 100;
+      const presenteValido = 1 <= +portaPremiada && +portaPremiada <= +qntPortas;
+      setIsValido(qtdeValida && presenteValido);
+    }
+  }, [portas]);
 
   useEffect(() => {
     if (router.query?.params) {
@@ -29,7 +38,13 @@ export default function Jogo() {
 
   return (
     <div className={styles.oJogo}>
-      <div className={styles.portas}>{renderPortas()}</div>
+      <div className={styles.portas}>
+        {isValido ? (
+          renderPortas()
+        ) : (
+          <h1 style={{ color: "white", fontSize: "3rem" }}>Valores inválidos</h1>
+        )}
+      </div>
       <div className={styles.botoes}>
         <Link href="/">
           <button>Reiniciar</button>
