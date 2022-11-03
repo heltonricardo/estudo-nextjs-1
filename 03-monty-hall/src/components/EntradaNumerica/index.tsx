62a748a67828cad028587fd76d23a15739d86f3b
heltonricardo <helton.ricardo13@gmail.com>
2022-11-03 16:12:40 -0300

    permitindo alteracao com on change

--- 03-monty-hall/src/components/EntradaNumerica/index.tsx
+++ 03-monty-hall/src/components/EntradaNumerica/index.tsx
@@ -7,10 +7,21 @@ interface Props {
 }
 
 export default function EntradaNumerica({ text, value, onChange }: Props) {
+  const dec = () => onChange(value - 1);
+  const inc = () => onChange(value + 1);
+  
   return (
     <div className={styles.entradaNumerica}>
       <span className={styles.texto}>{text}</span>
       <span className={styles.valor}>{value}</span>
+      <div className={styles.botoes}>
+        <button className={styles.btn} onClick={dec}>
+          -
+        </button>
+        <button className={styles.btn} onClick={inc}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
