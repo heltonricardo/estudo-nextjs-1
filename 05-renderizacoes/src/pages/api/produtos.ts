bd5317c8bacc534229426a1f4fbefeb8945a8585
heltonricardo <helton.ricardo13@gmail.com>
2022-11-07 14:58:07 -0300

    conteudo estatico com dependencia de requisicao

---
+++ 05-renderizacoes/src/pages/api/produtos.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Produto[]>) {
+  res.status(200).json([
+    { id: 1, nome: "Caneta", preco: 4.5 },
+    { id: 2, nome: "Caderno", preco: 20.3 },
+    { id: 3, nome: "Borracha", preco: 6.45 },
+    { id: 4, nome: "Tesoura", preco: 14.99 },
+  ]);
+}
