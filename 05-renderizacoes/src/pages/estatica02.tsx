15d751eccb0a77ad67397ff3c1dfabfd393f9109
heltonricardo <helton.ricardo13@gmail.com>
2022-11-07 16:46:50 -0300

    adicioanndo explicacao para estatica 2

--- 05-renderizacoes/src/pages/estatica02.tsx
+++ 05-renderizacoes/src/pages/estatica02.tsx
@@ -19,7 +19,12 @@ export default function Estatica02(props: Props) {
   );
 }
 
-/* O valor carregado na props será gerado em momento de compilação. Toda
+/* Quando exportamos uma função com nome getStaticProps, o next entende que
+ * a página é gerada de forma estática. Essa estratégia é usada quando não há
+ * necessidade de personalizar a página para cada usuário que faz a requisição.
+ * O valor carregado na props será gerado em momento de compilação. Toda
  * requisição receberá a mesma página (só é possível notar isso em modo de
  * produção - npm run build / npm start).
+ *
+ *  Ver: https://gist.github.com/heltonricardo/2007f05975d0ccbd3c0e5e9bec6a7b61
  */
