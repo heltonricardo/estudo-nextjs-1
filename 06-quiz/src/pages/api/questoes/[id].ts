87623ce9f10d10de54fefbf97cd60757a9a8a412
heltonricardo <helton.ricardo13@gmail.com>
2022-11-10 14:35:00 -0300

    retornando questao por id com tratamento de erro

--- 06-quiz/src/pages/api/questoes/[id].ts
+++ 06-quiz/src/pages/api/questoes/[id].ts
@@ -5,5 +5,8 @@ import BancoQuestoes from "../bancoQuestoes";
 type Data = IQuestao;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json(BancoQuestoes[0].paraObjeto());
+  const id = Number(req.query.id);
+  const questao = BancoQuestoes.find((q) => q.id === id);
+  questao && res.status(200).json(questao.paraObjeto());
+  res.status(404).end();
 }
