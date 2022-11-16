732ea53d6cfe2fccddb40d6632b783d47434a217
heltonricardo <helton.ricardo13@gmail.com>
2022-11-16 17:51:33 -0300

    finalizando estilo e animacao

--- 06-quiz/src/components/Resposta/index.tsx
+++ 06-quiz/src/components/Resposta/index.tsx
@@ -9,32 +9,31 @@ interface Props {
   respostaFornecida: (indice: number) => void;
 }
 
-export default function Resposta({ resposta, indice, letra, corFundo, respostaFornecida }: Props) {
+export default function Resposta(props: Props) {
+  const respostaRevelada = props.resposta.isRevelada ? styles.respostaRevelada : "";
+
   return (
-    <div className={styles.resposta} onClick={() => respostaFornecida(indice)}>
-      <div className={styles.conteudo}>
-        {resposta.isRevelada ? (
-          <div className={styles.verso}>
-            {resposta.isCorreta ? (
-              <div className={styles.certa}>
-                <span className={styles.titulo}>Acertou! 🤩</span>
-                <span className={styles.texto}>{resposta.texto}</span>
-              </div>
-            ) : (
-              <div className={styles.errada}>
-                <span className={styles.titulo}>Errou! 😵‍💫</span>
-                <span className={styles.texto}>{resposta.texto}</span>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className={styles.frente}>
-            <span className={styles.letra} style={{ backgroundColor: corFundo }}>
-              {letra}
-            </span>
-            <span className={styles.texto}>{resposta.texto}</span>
-          </div>
-        )}
+    <div className={styles.resposta} onClick={() => props.respostaFornecida(props.indice)}>
+      <div className={`${respostaRevelada} ${styles.conteudo}`}>
+        <div className={styles.frente}>
+          <span className={styles.letra} style={{ backgroundColor: props.corFundo }}>
+            {props.letra}
+          </span>
+          <span className={styles.texto}>{props.resposta.texto}</span>
+        </div>
+        <div className={styles.verso}>
+          {props.resposta.isCorreta ? (
+            <div className={styles.certa}>
+              <span className={styles.titulo}>Acertou! 🤩</span>
+              <span className={styles.texto}>{props.resposta.texto}</span>
+            </div>
+          ) : (
+            <div className={styles.errada}>
+              <span className={styles.titulo}>Errou! 😵‍💫</span>
+              <span className={styles.texto}>{props.resposta.texto}</span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
